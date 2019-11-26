8a24fc490f331008c412c1f6b05f7d1d31c0e46d
HARYSSON SOARES RODRIGUES DE QUEIROZ <queiroz.rodrigues@academico.ifrn.edu.br>
2019-11-26 11:26:46 -0300

    loading screen

---
+++ splash.asm
@@ -0,0 +1,739 @@
+.text
+#---------------------------------------------------------------------------------#
+#				SPLASH DA TELA
+#---------------------------------------------------------------------------------#
+main: 
+		lui $7, 0x1001
+      		addi $7, $7, 11036
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 50
+		addi $11, $0, 50
+		jal quadrado
+
+		lui $7, 0x1001
+      		addi $7, $7, 12624
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 10
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 13904
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 5
+		addi $11, $0, 1
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 12644
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 10
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 12664
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 4
+		addi $11, $0, 1
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 12916
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 4
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 13944
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 4
+		addi $11, $0, 1
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 14212
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 4
+		jal quadrado
+		
+		lui $7, 0x1001
+      		addi $7, $7, 14964
+      		addi $9, $0, 0x00ff0000 #0x00ff0000                    
+		addi $10, $0, 4
+		addi $11, $0, 1
+		jal quadrado
+		
+loading:	lui $7, 0x1001
+      		addi $7, $7, 16512
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17032
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado		
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17552
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18068
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18836
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 19340
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 19844
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 20348
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+
+		lui $7, 0x1001
+      		addi $7, $7, 19828
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 19308
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18788
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18020
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17512
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17008
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 16504
+      		addi $9, $0, 0x0000ff00 #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		#-----------------------------------#
+		
+		lui $7, 0x1001
+      		addi $7, $7, 16512
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17032
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado		
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17552
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18068
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18836
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 19340
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 19844
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 20348
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+
+		lui $7, 0x1001
+      		addi $7, $7, 19828
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 19308
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18788
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 18020
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17512
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 17008
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		lui $7, 0x1001
+      		addi $7, $7, 16504
+      		addi $9, $0, 0x00ff00ff #0x00ff0000                    
+		addi $10, $0, 1
+		addi $11, $0, 1
+		jal quadrado
+		
+		addi $2, $0, 32
+		addi $4, $0, 50
+		syscall
+		
+		j loading
+deuCerto:		
+      		addi $2, $0, 10
+      		syscall
+      
+      
+
+      
+#--------------------------------------------------------------------------------#
+#					FUNCTIONS
+#--------------------------------------------------------------------------------#
+#entradas
+#5 --> 16 - endereço
+#9 --> 17 - cor
+#10--> 18 - número de colunas
+limparBaixo:	sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		addi $29, $29, -20
+      		
+      		add $16, $0, $5
+            	add $17, $0, $9
+            	add $18, $0, $10
+            	
+forLimpar:  	beq $19, $18, exitLimpar
+      		sw $17, 0($16)
+      		add $16, $16, 4
+      		addi $19, $19, 1
+      		j forLimpar
+      		
+exitLimpar:     addi $29, $29, +20
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
+
+#--------------------------------------------------------------------------------#
+#entradas
+#7 --> 16 - endereço
+#9 --> 17 - cor
+#10--> 18 - número de colunas
+#11--> 19 - número de linhas
+
+#variáveis internas
+#20 - pula linha
+#21 - contador
+#22 - comparador
+
+
+abaD:		sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		sw $20, -20($29)
+      		addi $29, $29, -24
+      		
+      		add $16, $0, $7
+            	add $17, $0, $9
+            	add $18, $0, $10
+            	add $19, $0, $11
+      		addi $22, $0, 2
+      		
+      		
+forAbaD:	beq $19, $0, fimabaD
+		addi $20, $0, 64
+		addi $21, $21, 1
+		beq $21, $22, proxLinhaAbaD
+		sw $17, 0($16)
+		addi $16, $16, 4
+		j forAbaD
+
+proxLinhaAbaD:	addi $19, $19, -1
+            	sub $20, $20, $21
+		addi $21, $0, 0
+		addi $22, $22, 1
+pulandoLinhaAbaD:addi $20, $20, -1
+		addi $16, $16, 4
+		beq $20, 0, forAbaD
+		j pulandoLinhaAbaD
+      		
+      		
+fimabaD: 	addi $29, $29, +24
+		lw $20, +20($29)
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
+#--------------------------------------------------------------------------------#
+#entradas
+#7 --> 16 - endereço
+#9 --> 17 - cor
+#10--> 18 - número de colunas
+#11--> 19 - número de linhas
+
+#variáveis internas
+#20 - pula linha
+#21 - contador
+#22 - comparador		
+abaE:		sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		sw $20, -20($29)
+      		addi $29, $29, -24
+      		
+      		add $16, $0, $7
+            	add $17, $0, $9
+            	add $18, $0, $10
+            	add $19, $0, $11
+      		addi $22, $0, 1
+      		
+forAbaE:	beq $19, $0, fimabaE
+		beq $21, $22, proxLinhaAbaE
+		addi $21, $21, 1
+      		addi $20, $0, 64
+		sw $17, 0($16)
+		addi $16, $16, 4
+		j forAbaE
+
+proxLinhaAbaE:	addi $19, $19, -1
+		sub $20, $20, $21
+		addi $21, $0, 0
+		addi $22, $22, 1
+pulandoLinhaAbaE:
+		addi $20, $20, -1
+		addi $16, $16, 4
+		beq $20, 0, forAbaE
+		j pulandoLinhaAbaE
+      		
+      		
+fimabaE: 	addi $29, $29, +24
+		lw $20, +20($29)
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
+
+#--------------------------------------------------------------------------------#
+
+#7 --> 16 - endereço
+#9 --> 17 - cor
+#11--> 18 - número de pixels
+#19 - contador 
+      	
+line:  		sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		addi $29, $29, -20
+    
+            	add $16, $0, $7
+            	add $17, $0, $9
+            	add $18, $0, $11
+            	add $19, $0, $0
+            
+for:  		beq $19, $18, exit
+      		sw $17, 0($16)
+      		add $16, $16, 4
+      		addi $19, $19, 1
+      		j for
+      
+exit:      	addi $29, $29, +20
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
+#--------------------------------------------------------------------------------#
+#7 --> 16 - endereço
+#9 --> 17 - cor
+#11--> 18 - número de pixels
+#19 - contador
+#20 - contador2
+#21 - meio (58)
+      
+collumns:  	sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		sw $20, -20($29)
+      		sw $21, -24($29)
+      		addi $29, $29, -28
+    
+            	add $16, $0, $7
+            	add $17, $0, $9
+            	add $18, $0, $11
+            	add $19, $0, $0
+            	addi $20, $0, 3
+            	addi $21, $0, 58
+            
+for2:  		beq $19, $18, exit2
+		beq $20, 1, print
+		beq $20, 2, print
+		beq $20, 3, print
+		beq $20, 4, print
+		beq $20, 5, print
+		beq $20, 6, print
+		addi $21, $21, -1
+		beq $21, 0, reset
+cont:		add $16, $16, 4
+      		addi $19, $19, 1
+      		j for2
+
+reset:		addi $20, $0, 6
+            	addi $21, $0, 58
+		j cont
+		
+print:		sw $17, 0($16)
+		addi $20, $20, -1
+		j cont
+                  
+exit2:      	addi $29, $29, +28
+		lw $21, +24($29)
+		lw $20, +20($29)
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
+#--------------------------------------------------------------------------------#
+#$16 - endereço
+#$17 - cor
+#$18 - número de pixels
+#$19 - quebrador de linha
+#$20 - num de linha
+#$22 - num de saidas do tubo
+#$21 - contador 
+
+tubo:		sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		sw $20, -20($29)
+      		sw $21, -24($29)
+      		sw $22, -28($29)
+      		addi $29, $29, -32
+      		
+   		addi $17, $0, 0x00000000
+   		lui $16, 0x1001
+   		addi $16, $16, 1000
+   		addi $21, $0, 4
+
+forTubo: 	beq $19, 41, resetTubo
+		beq $21, 1, contTubo
+		beq $21, 2, contTubo
+		beq $21, 3, contTubo
+		beq $21, 4, contTubo
+		beq $21, $0, roxo
+contTubo:	sw $17, 0($16)
+      		addi $16, $16, 256
+      		addi $21, $21, -1
+      		addi $19, $19, 1
+      		beq $23, 3, fimTubo
+      		j forTubo
+      		
+resetTubo:	addi $17, $0, 0x00000000
+   		addi $21, $0, 4
+   		addi $19, $0, 0
+   		addi $23, $23, 1
+   		j forTubo
+   		
+roxo:		addi $17, $0, 0x00ff00ff
+		j contTubo		   		
+
+fimTubo:	addi $29, $29, +32
+		lw $22, +28($29)
+		lw $21, +24($29)
+		lw $20, +20($29)
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
+		
+#--------------------------------------------------------------------------------#
+#entradas
+#7 --> 16 - endereço
+#9 --> 17 - cor
+#10--> 18 - número de colunas
+#11--> 19 - número de linhas
+#12--> 20 - contador2
+
+#variáveis internas
+#21 - pula linha
+
+
+quadrado:	sw $31, 0($29)
+      		sw $16, -4($29)
+      		sw $17, -8($29)
+      		sw $18, -12($29)
+      		sw $19, -16($29)
+      		sw $20, -20($29)
+      		addi $29, $29, -24
+      		
+      		add $16, $0, $7
+            	add $17, $0, $9
+            	add $18, $0, $10
+            	add $19, $0, $11
+      		
+forQuadrado:	beq $19, $0, fimQuadrado
+		addi $21, $0, 64
+		sw $17, 0($16)
+		addi $16, $16, 4
+		addi $18, $18, -1
+		beq $18, $0, proxLinha
+		j forQuadrado
+
+proxLinha:	addi $19, $19, -1
+            	sub $21, $21, $10
+            	add $18, $0, $10
+pulandoLinha:	addi $21, $21, -1
+		addi $16, $16, 4
+		beq $21, 0, forQuadrado
+		j pulandoLinha
+		
+fimQuadrado:	addi $29, $29, +24
+		lw $20, +20($29)
+		lw $19, +16($29)
+		lw $18, +12($29)
+		lw $17, +8($29)
+		lw $16, +4($29)
+		lw $31, 0($29)
+		jr $31
